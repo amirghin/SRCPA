698d456a7b98ef5680b3a3936bee97614c5a8f84
Fabiola <fab.tames@gmail.com>
2014-12-14 13:26:42 -0600

    Conexion

--- DatabaseConnector.php
+++ DatabaseConnector.php
@@ -1,12 +1,12 @@
 <?php
-$con = mysqli_connect("localhost","root","1234","Veterinaria"); //1234 es el password mio
+//$con = mysqli_connect("localhost","root","1234","Veterinaria"); //1234 es el password mio
 
 // Check connection
-if (mysqli_connect_errno())
+/*if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }else{
-  	echo "Connected to database";
+  	echo "Connected to database"."<br>";
   }
 
 	$sql = "SELECT idrol, nombre FROM roles";
@@ -20,5 +20,26 @@ if (mysqli_connect_errno())
 	} else {
 	    echo "0 results";
 	}
-	$con->close();
+	$con->close();*/
+
+class DatabaseConnector{
+	public function __construct(){
+		$server: "localhost";
+		$user: "root";
+		$pass: "1234";
+		$db: "Veterinaria";
+		mysql_connect($server, $user, $pass) or die("Error connecting to sql server: ".mysql_error());
+		mysql_select_db($db);
+	}
+
+	function login($username, $password){
+		$sql = "SELECT password FROM usuarios WHERE nombre='".$username."'";
+
+	}
+
+
+
+}
+
+
 ?>
