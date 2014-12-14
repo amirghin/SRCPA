5ae51d9e8fc85a5014d5038c2fff6899fa6580ea
Fabiola <fab.tames@gmail.com>
2014-12-14 11:56:53 -0600

    Database connection working

--- DatabaseConnector.php
+++ DatabaseConnector.php
@@ -8,4 +8,17 @@ if (mysqli_connect_errno())
   }else{
   	echo "Connected to database";
   }
+
+	$sql = "SELECT idrol, nombre FROM roles";
+	$result = $con->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo "idrol: " . $row["idrol"]. " - Name rol: " . $row["nombre"]. " " . "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}
+	$con->close();
 ?>
