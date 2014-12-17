4fb66850cc3d1331bacb10ad4969cce527be872f
Amirghin <adrian@aastorga.com>
2014-12-17 00:43:58 -0600

    Crear Mascota y Expediente

--- crear_expediente.php
+++ crear_expediente.php
@@ -1,28 +1,31 @@
 <?php
-
-
 Include "Owner.php";
 Include "Pet.php";
 Include "Expedient.php";
 Include "header.php";
 Include "DatabaseConnector.php";
 ?>
-	<h2>Formulario Creado Exitosamente</h2> 
-<?php	
-	//$owner = new Owner($_POST["nombre"], $_POST["apellido"], $_POST["telefono"], $_POST["email"], $_POST["cedula"]);
-	$pet = new Pet($_POST["nombre_mascota"], $_POST["tipo"], $_POST["raza"], $_POST["fecha_nacimiento"], $_POST["dueno"]);
-	//echo $owner->getName();
-	echo $pet->getName();
-	echo $pet->getBirthdayDate();
-	echo $pet->getOwner();
-	echo $pet->sendToDatabase();
-	$con->close();
-	/*$insert_Mascota = "INSERT INTO Animal (nombre, tipo, raza, fechaNacimiento, Dueno_idDueno) VALUES ({'$pet->getName()'},{'$pet->getType()'}, {'$pet->getBreed()'} , {'$pet->getBirthdayDate()'},{'$pet->getOwner()'})";
-	if (mysqli_query($con, $insert_Mascota)) {
-		echo "Animal Creado de manera exitosa";
-	} else {
-		echo "Error: " . mysqli_error($con);
-	}
+ <h2>Formulario Creado Exitosamente</h2> 
+<?php 
 
-	$con->close();*/
+ $pet = new Pet($_POST["nombre_mascota"], $_POST["tipo"], $_POST["raza"], $_POST["fecha_nacimiento"], $_POST["dueno"]);
+ echo $pet->sendToDatabase();
+ $nombre = $_POST["nombre_mascota"];
+ $id_dueno= $_POST["dueno"];
+ //echo $nombre;
+ //echo $id_dueno;
+ $query_expediente = "SELECT a.idAnimal FROM Animal a, Dueno d WHERE d.idDueno = '${id_dueno}' AND a.nombre = '${nombre}'";
+ $info_id = mysqli_query($con, $query_expediente);
+ //$id_animal = mysqli_fetch(mysqli_query($con, $query_expediente));
+
+ while ($row=mysqli_fetch_assoc($info_id))
+    {
+     $query_crear_expediente = "INSERT into Expediente(Animal_idAnimal) values (${row['idAnimal']})";
+     mysqli_query($con, $query_crear_expediente);
+    }
+
+ //echo $id_animal;
+
+ $con->close();
+
 ?>
