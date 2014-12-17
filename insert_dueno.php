8e3178b8dd555212f0f457903b19751136c1271c
Amirghin <adrian@aastorga.com>
2014-12-17 11:57:26 -0800

    adding segundo apellido

--- insert_dueno.php
+++ insert_dueno.php
@@ -7,10 +7,11 @@ $apellido = $_POST['apellido'];
 $telefono = $_POST['telefono'];
 $email = $_POST['email'];
 $cedula = $_POST['cedula'];
+$apellido2 = $_POST['segundo_apellido'];
 
 
-$insert_user = "INSERT INTO Dueno (nombre, telefono, email, cedula, apellido1) 
-				VALUES ('${nombre}','${telefono}', '${email}', '${cedula}', '${apellido}')";
+$insert_user = "INSERT INTO Dueno (nombre, telefono, email, cedula, apellido1, apellido2) 
+				VALUES ('${nombre}','${telefono}', '${email}', '${cedula}', '${apellido}', '${segundo_apellido}')";
 $check_user = "SELECT idDueno FROM Dueno WHERE nombre = '${nombre}' AND cedula = '${cedula}'";
 
 if (mysqli_num_rows(mysqli_query($con,$check_user)) > 0 ){
